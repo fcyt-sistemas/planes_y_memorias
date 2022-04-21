f0432c9dbcdca4a9c3498f8eb8c3f3e0b4626c0e
alfredochoternasty <alfredochoternasty@gmail.com>
2022-04-21 02:30:59 -0300

    modificacion para login via webservices guarani

--- app/User.php
+++ app/User.php
@@ -16,8 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-       // 'name', 'email', 'password', 'docente_id',
-       'name', 'nro_documento', 'password', 'docente_id',
+       'name', 'email', 'password', 'docente_id',
+       // 'name', 'nro_documento', 'password', 'docente_id',
     ];
 
     /**
@@ -49,29 +49,29 @@ class User extends Authenticatable
         abort(401, 'Esta acción no está autorizada.');
     }
 
-public function hasAnyRole($roles)
-{
-    if (is_array($roles)) {
-        foreach ($roles as $role) {
-            if ($this->hasRole($role)) {
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
                 return true;
             }
         }
-    } else {
-        if ($this->hasRole($roles)) {
+        return false;
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
             return true;
         }
+        return false;
     }
-    return false;
-}
-
-public function hasRole($role)
-{
-    if ($this->roles()->where('name', $role)->first()) {
-        return true;
-    }
-    return false;
-}
 
     public function sendPasswordResetNotification($token)
     {
