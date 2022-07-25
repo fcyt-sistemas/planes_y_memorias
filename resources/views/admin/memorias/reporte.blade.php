077777c70d268bf140f07f4bd0ad8afc95f096af
aldanazualet <aldanazualet02@outlook.com>
2022-07-25 18:06:46 -0300

    kk

--- resources/views/admin/memorias/reporte.blade.php
+++ resources/views/admin/memorias/reporte.blade.php
@@ -1,29 +1,58 @@
-<!doctype html>
-<html lang="es">
+<html>
     <head>
         <meta charset="utf-8">
-        <title>Programa de contenidos extraído de las Memorias de cátedra</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <img style="width:30%;" src="data:image/png;base64,{{ $image }}"></img>
+        <style>
+            /** Define the margins of your page **/
+            @page {
+                margin: 200px 25px 50px 50px; /** arriba derecha abajo izquierda**/
+            }
+
+            header {
+                position: fixed;
+                top: -150px;
+                left: 0px;
+                /*right: 0px;
+                height: 150px;
+                bottom: 100px;*/
+            }
+            
+        </style>
     </head>
-    <body>   
-        @foreach($memorias as $m)
-            <table style="border-width: black">
-                <tr><td colspan="3"><b>ESTADO:</b>   
-                    @if($m->entregado) {!!link_to_action('MemoriaController@reporte', $title = 'ENTREGADA', $parameters = $m->id, $attributes = ['class'=>'entreg'])!!} @endif
-                    @if($m->observado) {!!link_to_action('MemoriaController@reporte', $title = 'REVISADA', $parameters = $m['id'], $attributes = ['class'=>'rev'])!!} @endif 
-                    @if($m->aprobado) {!!link_to_action('MemoriaController@reporte', $title = 'APROBADA', $parameters = $m['id'], $attributes = ['class'=>'aprob'])!!} @endif 
-                 <br>
-                </td></tr>
-                <tr><td colspan="3"><b>Carrera:</b> {{$m->carrera->nombre}}<br></td></tr>
-                <tr><td colspan="3"><b>Sede:</b> {{$m->sede->nombre}}<br></td></tr>
+    <body>
+        <header>
+            <img style="width:30%;" src="data:image/png;base64,{{ $image }}">
+            <h2 style="text-align: center;">Reporte de Memorias de las Cátedras</h2>
+        </header>
+
+        <main>
+            <table style="width: 700px;">
+                <?php $c = ""; ?>
+                @foreach ($memorias as $m)
+                @if($m['anio_academico'].$m['sede'].$m['carrera'] != $c)
                 <tr>
-                    <td><b>Materia:</b> {{$m->catedra->nombre}}<br></td>
-                    <td><b>Equipo Docente: </b> {{$m->equipo_docente}}<br></td>
-                    <td><b>Año Academico:</b> {{$m->anio_academico}}<br></td>
+                    <td colspan="3" style="text-align:center;  border-width:1px;">
+                        <b>AÑO ACADEMICO:    </b> {{ $m['anio_academico']}}
+                        <br><b>CARRERA:   </b> {{$m['carrera']}}
+                        <br><b>SEDE:   </b> {{$m['sede']}}
+                    </td>
                 </tr>
-            </table>      
-        @endforeach      
+                <tr>
+                    <td style="text-align:center; border:1px solid #000;"><b>MATERIA</b> </td>
+                    <td style="text-align:center; border:1px solid #000;"><b>EQUIPO DOCENTE </b></td>
+                    <td style="text-align:center; border:1px solid #000;"><b>ESTADO</b> </td>
+                </tr>
+            @endif
+            <tr>
+            <td style="text-align:left; border:1px solid #000;"> {{$m['catedra']}}</td>
+            <td style="text-align:left; border:1px solid #000;"> {{$m['equipo_docente']}}</td>
+            <td style="text-align:center; border:1px solid #000;"> {{$m['estado']}}</td>
+            </tr>
+            <?php 
+                $c = $m['anio_academico'].$m['sede'].$m['carrera'];
+            ?>
+                @endforeach
+            </table> 
+        </main>
     </body>
 </html>
--- resources/views/admin/planificaciones/reporte.blade.php
+++ resources/views/admin/planificaciones/reporte.blade.php
@@ -1,55 +1,58 @@
-<!doctype html>
-<html lang="es">
+<html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <title>Programa de contenidos extraído de la Planinificación de cátedra</title>
-        <img style="width:30%;" src="data:image/png;base64,{{ $image }}"></img>
-    </head>
-<body>
-    <h3 style="text-align: center;"><b>Reporte de Planificaciones de las Cátedras</b></h3>
-    @if(count($plani) === 0)
-        <div class="alert alert-success" role="alert">
-            No hay elementos cargados
-        </div>
-    @endif  
-    @foreach($plani as $p)
-        <br>
-        @while(!@empty($p))
-            @if($p['anio_academico']==$p['anio_academico'-1] && $p['sede']== $p['sede'-1] &&  $p['carrera']== $p['carrera'-1])
-                <table border="1px" style="width: 700px; background: #d3d5db;">
-                    <tr>
-                        <td style="text-align:center;"><b>MATERIA</b> </td>
-                        <td style="text-align:center;"><b>EQUIPO DOCENTE </b></td>
-                        <td style="text-align:center;"><b>ESTADO:</b> </td>
-                    </tr>
-                    <tr> 
-                       <td style="text-align:center;"> {{$p['catedra']}}</td>
-                       <td style="text-align:center;"> {{$p['equipo_docente']}}</td>
-                       <td style="text-align:center;"> {{$p['estado']}}</td>
-                    </tr>
-                </table>
-            @else
-                <table border="1px" style="width: 700px; background: #d3d5db;">
-                    <tr><td colspan="3" style="text-align:center;"><b>AÑO ACADEMICO:    </b> {{$p['anio_academico']}}</td></tr>
-                    <tr><td colspan="3" style="text-align:center;"><b>CARRERA:   </b> {{$p['carrera']}}</td></tr>
-                    <tr><td colspan="3" style="text-align:center;"><b>SEDE:   </b> {{$p['sede']}}</td></tr>
-                    <tr>
-                        <td style="text-align:center;"><b>MATERIA</b> </td>
-                        <td style="text-align:center;"><b>EQUIPO DOCENTE </b></td>
-                        <td style="text-align:center;"><b>ESTADO:</b> </td>
-                    </tr>
-                    <tr> 
-                       <td style="text-align:center;"> {{$p['catedra']}}</td>
-                       <td style="text-align:center;"> {{$p['equipo_docente']}}</td>
-                       <td style="text-align:center;"> {{ $p['estado'] }}</td>
-                    </tr>
-                </table>   
-            @endif 
-            @endempty
-        @endwhile
-    @endforeach
+        <style>
+            /** Define the margins of your page **/
+            @page {
+                margin: 200px 25px 50px 50px; /** arriba derecha abajo izquierda**/
+            }
 
-</body>
+            header {
+                position: fixed;
+                top: -150px;
+                left: 0px;
+                /*right: 0px;
+                height: 150px;
+                bottom: 100px;*/
+            }
+            
+        </style>
+    </head>
+    <body>
+        <header>
+            <img style="width:30%;" src="data:image/png;base64,{{ $image }}">
+            <h2 style="text-align: center;">Reporte de Memorias de las Cátedras</h2>
+        </header>
+
+        <main>
+            <table style="width: 700px;">
+                <?php $c = ""; ?>
+                @foreach ($plani as $p)
+                @if($p['anio_academico'].$p['sede'].$p['carrera'] != $c)
+                <tr>
+                    <td colspan="3" style="text-align:center;  border-width:1px;">
+                        <b>AÑO ACADEMICO:    </b> {{ $p['anio_academico']}}
+                        <br><b>CARRERA:   </b> {{$p['carrera']}}
+                        <br><b>SEDE:   </b> {{$p['sede']}}
+                    </td>
+                </tr>
+                <tr>
+                    <td style="text-align:center; border:1px solid #000;"><b>MATERIA</b> </td>
+                    <td style="text-align:center; border:1px solid #000;"><b>EQUIPO DOCENTE </b></td>
+                    <td style="text-align:center; border:1px solid #000;"><b>ESTADO</b> </td>
+                </tr>
+            @endif
+            <tr>
+            <td style="text-align:left; border:1px solid #000;"> {{$p['catedra']}}</td>
+            <td style="text-align:left; border:1px solid #000;"> {{$p['equipo_docente']}}</td>
+            <td style="text-align:center; border:1px solid #000;"> {{$p['estado']}}</td>
+            </tr>
+            <?php 
+                $c = $p['anio_academico'].$p['sede'].$p['carrera'];
+            ?>
+                @endforeach
+            </table> 
+        </main>
+    </body>
 </html>
